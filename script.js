95ef361a9450fda83f24acf53d6c20313d12ce82
Dev0312 <vishnukdev@outlook.com>
2021-12-16 14:25:10 +0530

    bug fixed header and feature section started

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+console.log("connected")
 var menuBtn = document.getElementById('menuBtn');
 var sideNav = document.getElementById("sidebar");
 var menu = document.getElementById('menu');
 
 menuBtn.onclick = function () {
+    console.log("clicked")
     if (sideNav.style.right == "-250px") {
         sideNav.style.right = '0'
         menu.src = "Images/Barber_Shop_img/close.png";
