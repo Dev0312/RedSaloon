09de47625c3afe443370fa571145635757e1101a
Dev0312 <vishnukdev@outlook.com>
2021-12-16 12:21:58 +0530

    Header Section Compleated and also responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+var menuBtn = document.getElementById('menuBtn');
+var sideNav = document.getElementById("sidebar");
+var menu = document.getElementById('menu');
+
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = '0'
+        menu.src = "Images/Barber_Shop_img/close.png";
+    } else {
+        sideNav.style.right = "-250px"
+        menu.src = "Images/Barber_Shop_img/menu.png";
+    }
+        
+}
